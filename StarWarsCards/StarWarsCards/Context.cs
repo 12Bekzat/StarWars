35f7ac8512743f733a1d57f320c02cf4b26c8482
Toleutai Bekzat <toleutaib@gmail.com>
2019-12-02 21:38:17 +0600

    Карты звездных воин

---
+++ StarWarsCards/StarWarsCards/Context.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarWarsCards
+{
+    public class Context : DbContext
+    {
+        private readonly string connectionString;
+        public Context(string connString)
+        {
+            Database.EnsureCreated();
+            connectionString = connString;
+        }
+
+        public DbSet<Result> Elements { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ StarWarsCards/StarWarsCards/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StarWarsCards
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string url = "https://swapi.co/api/people/*";
+        private const string CONNECTION_STRING = "Server=A-104-10;Database=StarWars;Trusted_Connection=True;";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Load(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(page.Text))
+            {
+                MessageBox.Show("Введите страницу!");
+            }
+            else
+            {
+                int pageNum = 0;
+                if (!int.TryParse(page.Text, out pageNum))
+                {
+                    MessageBox.Show("Введите число для страницы!");
+                    return;
+                }
+                if(pageNum == 0)
+                {
+                    MessageBox.Show("Ноль нельзя ввести!");
+                }
+                WebClient webClient = new WebClient();
+                url = url.Replace("*", page.Text);
+                string json = webClient.DownloadString(url);
+                Result root = JsonConvert.DeserializeObject<Result>(json);
+                cardsStarwars.Items.Add(root);
+            }
+        }
+    }
+}
